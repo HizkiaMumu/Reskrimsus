f50f11aed6b52cae70c74dfcfa6ac6e215583d55
Hizkia Mumu <hizkiamumu24@gmail.com>
2018-04-04 10:35:45 +0800

    Basic setup

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    @yield('style')
+  </head>
+  <body>
+    @yield('content')
+    
+    @yield('script')
+  </body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+@section('title', 'HOME | RESKRIMSUS POLDA KALTIM')
+@section('style')
+
+@endsection
+@section('content')
+
+  <h1>Welcome</h1>
+
+@endsection
+@section('script')
+
+@endsection
