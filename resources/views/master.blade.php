a27e3af55e0df591ea9dd5b341a3dfad5370dd45
Hizkia Mumu <hizkiamumu24@gmail.com>
2018-04-06 20:48:24 +0800

    fix homepage user interface.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,15 +1,159 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>@yield('title')</title>
-    @yield('style')
-  </head>
-  <body>
+<html lang="en" class="no-js">
 
-    @yield('content')
 
-    <!-- Custom Script HERE! -->
-    @yield('script')
-  </body>
+<!-- Mirrored from themesdesign.in/brizzle/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Apr 2018 07:46:20 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Brizzle | Responsive Bootstrap Landing Page Template</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Themesdesign" />
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Magnificpopup Css -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!--Material Icon -->
+    <link rel="stylesheet" type="text/css" href="css/materialdesignicons.min.css" />
+
+    <!-- themefy icon -->
+    <link rel="stylesheet" type="text/css" href="css/pe-icon-7-stroke.css" />
+
+    <!--Slider-->
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="css/owl.theme.css" />
+    <link rel="stylesheet" href="css/owl.transitions.css" />
+
+    <!-- Custom  Css -->
+    <link rel="stylesheet" type="text/css" href="css/style-template.css" />
+    <link href="css/colors/default.css" rel="stylesheet" id="color-opt">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+    <body>
+
+        <!-- Header start -->
+        <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light sticky">
+            <div class="container">
+
+               <!--  <a class="logo" href="#">
+                    <img src="images/logo-dark.png" alt="" class="img-fluid logo-dark" >
+                    <img src="images/logo-light.png" alt="" class="img-fluid logo-light" >
+                </a> -->
+
+                <a class="navbar-brand" href="#">Ditreskrimsus</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="mdi mdi-menu"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#home">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#htck">HTCK</a>
+                        </li>
+
+                        <li class="nav-item">
+                          <div class="btn-group">
+                            <a class="nav-link dropdown-toggle pointerHover" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              SUBDIT
+                            </a>
+                            <div class="dropdown-menu" style="background: #262829 !important;">
+                              <a class="dropdown-item" href="#">Subdit I</a>
+                              <!-- <div class="dropdown-divider"></div> -->
+                              <a class="dropdown-item" href="#">Subdit II</a>
+                              <!-- <div class="dropdown-divider"></div> -->
+                              <a class="dropdown-item" href="#">Subdit III (TIPIDKOR)</a>
+                              <!-- <div class="dropdown-divider"></div> -->
+                              <a class="dropdown-item" href="#">Subdit IV</a>
+                            </div>
+                          </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#strukturOrganisasi">Struktur Organisasi</a>
+                        </li>
+
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#subdit1">Subdit I</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#subdit2">Subdit II</a>
+                        </li>
+
+                         <li class="nav-item">
+                            <a class="nav-link" href="#subdit3">Subdit III</a>
+                        </li>
+
+                         <li class="nav-item">
+                            <a class="nav-link" href="#subdit4">Subdit IV</a>
+                        </li> -->
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#caseManagement">Case Management</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header end  -->
+
+        <!-- Home start -->
+         <section class="bg-home bg-home-full" id="home">
+            <div class="bg-dark-overlay"></div>
+            <div class="home-center">
+                <div class="home-desc-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="text-center text-white">
+                                    <img src="images/logo.png" alt="Logo" id="logoLanding">
+                                    <h1 class="home-title mx-auto mt-4">DIREKTORAT RESERSE KRIMINAL KHUSUS<br><b>POLDA KALIMANTAN TIMUR</b></h1>
+                                    <p class="home-subtitle mx-auto pt-2">Bersama mewujudkan pelayanan penyelidikan dan penyidikan yang profesional, prosedural, proposional, transparan, akuntabel, dan dapat dipercaya masyarakat.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- home end -->
+
+        <!-- Back to top -->
+        <a href="#" class="back-to-top" id="back-to-top"> <i class="mdi mdi-chevron-up"> </i> </a>
+
+        <!-- javascript -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/scrollspy.min.js"></script>
+        <!-- Owl Carousel -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- Magnific Popup -->
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <!-- switcher js -->
+        <script src="js/switcher.js"></script>
+        <!-- Custom js -->
+        <script src="js/app-template.js"></script>
+    </body>
+
+
+<!-- Mirrored from themesdesign.in/brizzle/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Apr 2018 07:46:45 GMT -->
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,13 +1,1 @@
 @extends('master')
-@section('title', 'HOME | RESKRIMSUS POLDA KALTIM')
-@section('style')
-
-@endsection
-@section('content')
-
-
-
-@endsection
-@section('script')
-
-@endsection
